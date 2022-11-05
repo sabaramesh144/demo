f0f681ab0bde2ebc805dfe4a7ed2a9313e8014e8
sabaramesh <sabaramesh144@gmail.com>
2022-11-05 12:13:05 +0530

    none

---
+++ Methodoverridding.java
@@ -0,0 +1,22 @@
+public class Methodoverridding extends Polymorphism {
+
+    void Father()
+    {
+ 	   
+ 	   System.out.println("Thar");
+ 	   
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Methodoverridding obj= new Methodoverridding();
+		
+		obj.Father();
+		
+	}
+
+}
+
+
+
