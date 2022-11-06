4a0a3ac5c1252d9efbfed5ebcb298cddf6acfa18
sabaramesh <sabaramesh144@gmail.com>
2022-11-06 11:47:11 +0530

    none

---
+++ Abstract.java
@@ -0,0 +1,11 @@
+public abstract class Abstract {
+      abstract void college();    
+      abstract void semster();
+      abstract void Branch();
+      void university() 
+      {
+    	  System.out.println("Anna university");  
+      }
+      
+}
+
